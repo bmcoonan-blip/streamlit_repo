08a356ac8735ed108cb9cb5cca317ebbd50432b3
bmcoonan-blip <bmcoonan@gmail.com>
2025-09-21 18:27:24 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
-import streamlit as st\
-\
-st.title("My First Streamlit App")\
-\
-st.header("Slider Example")\
-x = st.slider("Select a value")\
-st.write(x, "squared is", x * x)\
+{import streamlit as st
+
+st.title("My First Streamlit App")
+
+st.header("Slider Example")
+x = st.slider("Select a value")
+st.write(x, "squared is", x * x)
 }
